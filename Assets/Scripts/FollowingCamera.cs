731640c9d5b680e002a9e1365008e946e4132cf1
Piotr Kocia <p.kocia@wp.pl>
2023-03-17 07:38:46 +0100

    ARES-6: Implement following camera
    Added FollowingCamera and CameraTarget components. FollowingCamera
    copies the position of the assigned CameraTarget.

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera: MonoBehaviour {
+    [SerializeField]
+    private CameraTarget target;
+
+    private void Update() {
+        if(target == null) {
+            return;
+        }
+
+        Vector3 position = target.transform.position;
+        // Do NOT modify the Z coordinate.
+        position.z = transform.position.z;
+        transform.position = position;
+    }
+
+    public void AttachTo(CameraTarget target) {
+        this.target = target;
+        Update();
+    }
+}
